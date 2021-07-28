a8ba32afc25f5d98515e1a5519c6e3a5dc7982c9
Srishti Rajani <srishtirajani@gmail.com>
2021-07-28 11:23:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUDException
+{
+    public class PriceLessThan50Exception: Exception
+    {
+        public PriceLessThan50Exception(string message): base(message) { }
+    }
+    class Program
+    {
+        public static string connStr = @"Data Source=LAPTOP-IH3L07PH;Database=Assignment1DB;Integrated Security= true";
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(Program.connStr);
+            SqlDataReader reader = null;
+            conn.Open();
+            SqlCommand cmd2 = new SqlCommand("SELECT * FROM Item; ", conn);
+            try
+            {
+                reader = cmd2.ExecuteReader();
+                while (reader.Read())
+                {
+                    int val = Convert.ToInt32(reader[2].ToString());
+                    if (val > 50)
+                    {
+                        throw (new PriceLessThan50Exception("Price must be less than 50"));
+                    }
+                    Console.WriteLine("{0}  {1}  {2}  {3}", reader[0], reader[1], reader[2], reader[3].ToString().Split()[0]);
+                }
+            }
+            catch (PriceLessThan50Exception e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
+            
+            conn.Close();
+        }
+    }
+
+}
